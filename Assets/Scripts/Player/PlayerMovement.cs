08d954bcd22c088c87b39f343783508db00e1c09
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-19 22:41:03 -0300

    Several Changes
    TextMeshPro + Extras
    Player Jump
    Canvas & Button for Jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,9 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private float _jumpForce;
 	[SerializeField] private Rigidbody2D _rb2d;
 
-	private void FixedUpdate()
-	{
-
-	}
 	public void Jump()
 	{
+		if (_rb2d.velocity.y != 0) return;
 		_rb2d.AddForce(new Vector2(transform.position.x, _jumpForce), ForceMode2D.Impulse);
 	}
 
