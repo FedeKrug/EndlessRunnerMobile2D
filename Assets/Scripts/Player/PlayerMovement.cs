754d6f0760bb1dbd507e5c6470eae57cddbd870a
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-10-19 23:21:54 -0300

    MainMenu

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
 	public void Jump()
 	{
 		if (_rb2d.velocity.y != 0) return;
-		_rb2d.AddForce(new Vector2(transform.position.x, _jumpForce), ForceMode2D.Impulse);
+		_rb2d.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
 	}
 
 }
